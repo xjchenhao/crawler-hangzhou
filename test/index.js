14beb41af911c7308361a23a4375855b96e3dcdc
xjchenhao <wy.chenhao@qq.com>
2017-09-18 17:36:40 +0800

    测试地址修改

--- test/index.js
+++ test/index.js
@@ -2,15 +2,15 @@ const crawler = require('../index');
 const moment = require('moment');
 
 crawler({
-    moduleType: '',
-    isHtml:true,
-    dateAfter: moment(moment().format('YYYY-MM-DD'), 'YYYY-MM-DD').valueOf()
+  moduleType: '',
+  isHtml: false,
+  dateAfter: moment(moment().format('YYYY-MM-DD'), 'YYYY-MM-DD').valueOf()
 }).then((value) => {
-    console.log(value.map((obj) => {
-        return Object.assign(obj, {
-            date: moment(obj.moment).format('YYYY-MM-DD HH:mm:ss')
-        })
-    }));
+  console.log(value.map((obj) => {
+    return Object.assign(obj, {
+      date: moment(obj.moment).format('YYYY-MM-DD HH:mm:ss')
+    })
+  }));
 }).catch((err) => {
-    console.log(err);
+  console.log(err);
 });
